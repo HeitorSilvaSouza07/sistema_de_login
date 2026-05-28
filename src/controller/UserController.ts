a1118513fec421e14ecca983bf1238dcced2f05d
HeitorSilvaSouza77 <euclidesde@gmail.com>
2026-05-28 11:25:04 -0300

    feat: login

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -21,10 +21,10 @@ export class UserController{
 
             const userExisting = await repo.findOneBy({ emailUser: emailUser })
 
-            if(emailUser){
+            if(userExisting){
                 return res.status(400).json({
                     status: false,
-                    msg: 'O email já possue registro cadastrado'
+                    msg: 'O email já possui registro cadastrado'
                 })
             }
 
@@ -32,18 +32,21 @@ export class UserController{
 
             const user = repo.create({
                 emailUser: emailUser,
-                passwordUser: passwordUser
+                passwordUser: hashPassword
             })
 
             await repo.save(user)
 
             return res.status(201).json({
-                status: false,
+                status: true,
                 msg: 'Usuario criado com sucesso'
             })
 
         }catch(error: any){
-
+            return res.status(500).json({
+                status: false,
+                msg: 'Erro interno'
+            })
         }
     }
 
@@ -52,8 +55,55 @@ export class UserController{
 
             const { passwordUser, emailUser } = req.body;
 
+            if(!emailUser || !passwordUser){
+                return res.status(400).json({
+                    status: false,
+                    msg: 'Todos os campos devem ser preenchidos'
+                })
+            }
+
+            const repo = Connection.getRepository(User);
+
+            const user = await repo.findOneBy({ emailUser: emailUser })
+
+            if(!user){
+                return res.status(404).json({
+                    status: false,
+                    msg: 'Usuário não existe'
+                })
+            }
+
+            const hashDecript = await bcrypt.compare(String(passwordUser), user.passwordUser)
+
+            if(!hashDecript){
+                return res.status(400).json({
+                    status: false,
+                    msg: 'Senha invalida'
+                })
+            }
+
+                 const token = jwt.sign(
+                {
+                    id: user.id,
+                    email: user.emailUser
+                },
+                process.env.JWT_SECRET || 'secret',
+                { expiresIn: '1h' }
+            )
+
+            return res.status(201).json({
+                status: true,
+                msg: 'Login realizado com sucesso',
+                data:{
+                    token: token
+                } 
+            })
+
         }catch(error: any){
-            
+            return res.status(500).json({
+                status: false,
+                msg: 'Erro interno'
+            })
         }
     }
 }
